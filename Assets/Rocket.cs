22515627b554a0bed6b8a0490f3d40be89b7b185
Laura Huovinen <laura.huovinen@tuni.fi>
2021-01-20 22:12:08 +0200

    Added sound
    - Add: Rocket sounds

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class Rocket : MonoBehaviour
 {
     Rigidbody rigidBody;
+    AudioSource audioSource;
 
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     void Update()
@@ -17,11 +19,20 @@ public class Rocket : MonoBehaviour
     }
 
     private void ProcessInput() 
-    { 
+    {
         if (Input.GetKey(KeyCode.Space))
         {
             rigidBody.AddRelativeForce(Vector3.up);
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
         }
+        else
+        {
+            audioSource.Stop();
+        }
+
 
         if (Input.GetKey(KeyCode.A))
         {
